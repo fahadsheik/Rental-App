6e94a52944dc8972b06c898111d8955ca6f6a893
fahadsheik <fahad96644@gmail.com>
2023-12-25 12:08:32 +0530

    part

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:rent_360_app/home_page.dart';
+import 'package:rent_360_app/pages/home_page.dart';
 
 
 void main() {
